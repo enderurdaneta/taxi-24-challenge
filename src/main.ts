cabd634d0eb2ab93188d8e1dbbe5b0782be446c8
Ender Urdaneta <urdanetaenderjose@gmail.com>
2023-05-11 21:40:09 -0500

    feat(config): install swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taxi24 CHALLENGE')
+    .setDescription('The goal of this project is to create a public API REST.')
+    .setContact('Ender Urdaneta', '', 'urdanetaenderjose@gmail.com')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: 'RatherLabs CHALLENGE',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
